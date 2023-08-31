7ae0433cb9e06dd6ff835770e2c5273f0702fc3d
brickwall <sk292207@gmail.com>
2023-08-31 15:39:17 +0530

    Add tests for editing and checking api key

---
+++ spec/system/api_key_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+# include './support/LoginHelper'
+
+feature 'user able to edit youtube api key', type: :system do
+  include LoginHelper
+
+  it 'shows api key to users' do
+    User.create(email:'bla@bla.com', password: 'password', youtube_api_key: 'arandomnumber', name: 'Bla')
+
+    log_in_as('bla@bla.com')
+
+    sleep 2
+
+    visit '/api_key'
+
+    expect(page).to have_content("Your Api Key is '#{User.first.youtube_api_key}'")
+  end
+
+  it 'user able to edit api key' do
+    User.create(email:'bla@bla.com', password: 'password', youtube_api_key: 'arandomnumber', name: 'Bla')
+
+    log_in_as('bla@bla.com')
+
+    sleep 2
+
+    visit '/api_key/edit'
+    expect(page).to have_field('Api Key', with: 'arandomnumber')
+
+    fill_in 'Api Key', with: 'anotherrandomnumber'
+    click_button 'Update Key'
+
+    expect(page).to have_content('Your Api Key is anotherrandomnumber')
+  end
+
+end
