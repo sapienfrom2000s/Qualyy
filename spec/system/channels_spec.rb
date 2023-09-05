fd77b963e3e8472958bd4688099347a3627cd3c7
brickwall <sk292207@gmail.com>
2023-09-05 23:19:25 +0530

    Add test: Edit channel details

--- spec/system/channels_spec.rb
+++ spec/system/channels_spec.rb
@@ -67,4 +67,33 @@ feature 'user able to see filters', type: :system do
     
     expect(page).to_not have_content('abracadabra')
   end
+
+  it 'edits channel info when edit link is clicked' do
+    User.create(email: 'bla@bla.com', password: 'password', youtube_api_key: 'arandomnumber', name: 'Bla')
+
+    log_in_as('bla@bla.com')
+
+    sleep 2
+
+    visit '/channels'
+
+    click_link 'Add Channel'
+
+    fill_in 'Channel ID', with: 'abracadabra'
+    fill_in 'Keywords', with: 'keyword1;keyword2'
+    fill_in 'Non Keywords', with: 'nonkeyword1;nonkeyword2'
+    fill_in 'Published Before', with: '02122022'
+    fill_in 'Published After', with: '02112022'
+    fill_in 'Minimum Time(in s)', with: 100
+    fill_in 'Maximum Time(in s)', with: 1000
+    select '10', :from => 'Videos'
+    click_button 'Add'
+
+    click_link 'Edit'
+
+    expect(page).to have_field('Channel ID', with: 'abracadabra').and have_field('Keywords', with: 'keyword1;keyword2')
+      .and have_field('Non Keywords', with: 'nonkeyword1;nonkeyword2').and have_field('Published Before', with: '02122022')
+      .and have_field('Minimum Time(in s)', with: '100').and have_field('Maximum Time(in s)', with: '1000')
+      .and have_field('Videos', with: '10')
+  end
 end
