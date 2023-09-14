e570e1c0ecf5a1a0cbd57dff3d0764a08cb8af63
brickwall <sk292207@gmail.com>
2023-09-14 09:35:50 +0530

    Move test from video_spec to background_spec job
    Move test from video_spec to
    youtubeapi_call_to_fetch_channel_metadata_job_spec.
    The code to be tested also moved from model to Active Job.

--- spec/jobs/youtubeapi_call_to_fetch_channel_metadata_job_spec.rb
+++ spec/jobs/youtubeapi_call_to_fetch_channel_metadata_job_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe YoutubeapiCallToFetchChannelMetadataJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "with a string seperated by semicolons" do
+    it "checks whether apple and sky keywords are present in An apple floating in the sky" do
+      keywords = 'apple;sky'.split(';')
+      string = 'An apple in the sky'
+      expect(subject.send(:keywords_in_string?, {keywords: keywords, string: string})).to be true
+    end
+
+    it "checks whether apple and sky keywords are present in An Apple floating in the Cloud" do
+      keywords = 'apple;sky'.split(';')
+      string = 'An Apple in the Cloud'
+      expect(subject.send(:keywords_in_string?, {keywords: keywords, string: string})).to be false
+    end
+
+    it "checks whether official video and music keywords are present in Official Video | Some Artist | Music Video" do
+      keywords = 'official video;music'.split(';')
+      string = 'Official Video | Some Artist | Music Video'
+      expect(subject.send(:keywords_in_string?, {keywords: keywords, string: string})).to be true
+    end
+
+    it "checks whether apple and sky keyword are absent in An apple floating in the sky" do
+      keywords = 'apple;sky'.split(';')
+      string = 'An apple in the sky'
+      expect(subject.send(:keywords_not_in_string?, {keywords: keywords, string: string})).to be false
+    end
+
+    it "checks whether apple and sky keywords are absent in An Apple floating in the Cloud" do
+      keywords = 'apple;sky'.split(';')
+      string = 'An Apple in the Cloud'
+      expect(subject.send(:keywords_not_in_string?, {keywords: keywords, string: string})).to be false
+    end
+
+    it "checks whether official video and music keywords are absent in Movie video | Some Actor | official" do
+      keywords = 'official video;music'.split(';')
+      string = 'Movie video | Some Actor | official'
+      expect(subject.send(:keywords_not_in_string?, {keywords: keywords, string: string})).to be true
+    end
+  end
 end
