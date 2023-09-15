f81e0b99d352cc8d0538f25387dbf0b8033bc9ae
brickwall <sk292207@gmail.com>
2023-09-15 14:38:25 +0530

    Fix: faraday unable to handle multiple arguments of same name
    url&part=s1&part=s2, faraday overwrote part as s2, but it should
    have sent request for both s1 and s2. Had to make seperate request
    for s1 and s2 seperately

--- app/jobs/fetchvideometadata_job.rb
+++ app/jobs/fetchvideometadata_job.rb
@@ -4,35 +4,47 @@ class FetchvideometadataJob < ApplicationJob
   attr_reader :list
 
   after_perform do
-    FetchVideoDislikesJob.perform_later(list)
+    FetchVideoDislikesJob.perform_later(list, current_user)
   end
 
-  def perform(channel_list)
+  def perform(video_list, current_user)
+    @current_user = current_user
     @list = []
-    channel_list.each do |channel, video_list|
-      video_list.each do |video|
-        url = formurl(video['id']['videoId'], current_user.youtube_api_key)
-        begin
-          video_metadata = fetch(url)
-        rescue => exception
-          puts exception # display error in broadcast
-          next          
-        end
-        list << video_metadata if metadata_satisfies_duration_filter?(video_metadata, 
-          { minimum_duration: channel.filter.minimum_duration,
-          maximum_duration: channel.filter.maximum_duration })
+    video_list.each do |video|
+      urls = form_urls(video['id']['videoId'], current_user.youtube_api_key)
+      begin
+        video_metadata = fetch(urls)
+      rescue => exception
+        puts exception # display error in broadcast
+        next          
       end
+      video_metadata['id'] = video['id']['videoId']
+      list << video_metadata if metadata_satisfies_duration_filter?(video_metadata, 
+        { minimum_duration: video['minimum_duration'],
+        maximum_duration: video['maximum_duration'] })
     end
   end
 
   private
 
-  def formurl(videoId, api_key)
-    url = BASE_URL + '/videos?' + 'part=contentDetails&part=statistics&part=snippet' + "&id=#{videoId}" + "&key=#{api_key}"
+  def fetch(urls)
+    video_metadata = {}
+    urls.each do |key, url|
+      video_metadata[key] = super(url)['items'][0][key]
+    end
+    video_metadata
+  end
+
+  def form_urls(videoId, api_key)
+    {
+      'statistics' => BASE_URL + 'videos?' + '&part=statistics' + "&id=#{videoId}" + "&key=#{api_key}",
+      'contentDetails' => BASE_URL + 'videos?' + '&part=contentDetails' + "&id=#{videoId}" + "&key=#{api_key}",
+      'snippet' => BASE_URL + 'videos?' + '&part=snippet' + "&id=#{videoId}" + "&key=#{api_key}"
+    }
   end
 
   def metadata_satisfies_duration_filter?(metadata, args)
-    duration = ActiveSupport::Duration.parse(metadata['items'][0]['contentDetails']['duration']).to_s.to_i
+    duration = ActiveSupport::Duration.parse(metadata['contentDetails']['duration']).to_s.to_i
     duration >= args[:minimum_duration] && duration <= args[:maximum_duration]
   end
 end
