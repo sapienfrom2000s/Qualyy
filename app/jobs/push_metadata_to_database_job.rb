830faaff9bf5eb801c2a8541e7e3e1dc1d4b984f
brickwall <sk292207@gmail.com>
2023-09-15 14:46:01 +0530

    Fix: structure of list changed, so the way to access it should change

--- app/jobs/push_metadata_to_database_job.rb
+++ app/jobs/push_metadata_to_database_job.rb
@@ -1,19 +1,19 @@
 class PushMetadataToDatabaseJob < ApplicationJob
   queue_as :default
 
-  def perform(list)
-    # current_user.videos.destroy_all
+  def perform(list, current_user)
+    current_user.videos.destroy_all
     list.each do |video|
-      duration = ActiveSupport::Duration.parse(video['items'][0]['contentDetails']['duration'])
-      identifier = video['items'][0]['id']
-      published_on = Date.parse(video['items'][0]['snippet']['publishedAt'])
-      title = video['items'][0]['snippet']['title']
-      views = video['items'][0]['statistics']['viewCount']
-      comments = video['items'][0]['statistics']['commentCount']
-      dislikes = video['items'][0]['statistics']['dislikes']
+      duration = ActiveSupport::Duration.parse(video['contentDetails']['duration'])
+      identifier = video['id']
+      published_on = Date.parse(video['snippet']['publishedAt'])
+      title = video['snippet']['title']
+      views = video['statistics']['viewCount']
+      comments = video['statistics']['commentCount']
+      dislikes = video['statistics']['dislikes']
       video = Video.new(identifier: identifier, duration: duration,
          published_on: published_on, title: title, views: views,
-          comments: comments, dislikes: dislikes )
+          comments: comments, dislikes: dislikes, rating:  )
       video.user = current_user
       video.save
     end
