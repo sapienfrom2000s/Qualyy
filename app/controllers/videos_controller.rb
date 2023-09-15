19427f04bf1954ff6685a210fc5680f3c21f0c37
brickwall <sk292207@gmail.com>
2023-09-15 16:14:25 +0530

    index action to pass cached videos and new to request new list

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,6 +3,10 @@ class VideosController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @videos = current_user.videos.order(rating: :desc)
+  end
+
+  def new
     YoutubeapiCallToFetchChannelMetadataJob.perform_later current_user
   end
 end
