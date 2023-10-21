298d53dda092f683ab0411333e8c4641b3100028
brickwall <queenisuseless@gmail.com>
2023-10-21 12:18:26 +0530

    Integrate factory bot and devise helper

--- spec/system/api_key_spec.rb
+++ spec/system/api_key_spec.rb
@@ -1,37 +1,28 @@
 require 'rails_helper'
-# include './support/LoginHelper'
 
 feature 'user able to edit youtube api key', type: :system do
-  include LoginHelper
+  include Devise::Test::IntegrationHelpers
+
+  let(:user) { create(:user) }
 
   it 'shows api key to users' do
-    User.create(email: 'bla@bla.com', password: 'password', youtube_api_key: 'arandomnumber', name: 'Bla')
-
-    log_in_as('bla@bla.com')
-
-    sleep 2
+    sign_in user
 
     visit '/api_key'
 
-    expect(page).to have_content("Your API Key is '#{User.first.youtube_api_key}'")
+    expect(page).to have_content("Your API Key is '#{user.youtube_api_key}'")
   end
 
   it 'user able to edit api key' do
-    User.create(email: 'bla@bla.com', password: 'password', youtube_api_key: 'arandomnumber', name: 'Bla')
-
-    log_in_as('bla@bla.com')
-
-    sleep 2
+    sign_in user
 
     visit '/api_key/edit'
-    expect(page).to have_field('Youtube api key', with: 'arandomnumber')
+    expect(page).to have_field('Youtube api key', with: 'randomstring')
 
     fill_in 'Youtube api key', with: 'anotherrandomnumber'
     click_button 'Update'
 
-    sleep 2
-
     expect(page).to have_content('API Key successfully updated')
-    expect(page).to have_content("Your API Key is '#{User.first.youtube_api_key}'")
+    expect(page).to have_content("Your API Key is 'anotherrandomnumber'")
   end
 end
