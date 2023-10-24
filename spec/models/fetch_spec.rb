7779061c7ca34877c9cd4e8683602d295fbe44f1
brickwall <queenisuseless@gmail.com>
2023-10-24 18:16:05 +0530

    Make test stronger

--- spec/models/fetch_spec.rb
+++ spec/models/fetch_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe Fetch, type: :model do
     channelid = 'UC56gTxNs4f9xZ7Pa2i5xNzg'
     url = Fetch.channel_metadata_url(Rails.application.credentials.youtube_api_key, channelid, Date.new(2004,3,3).to_time.utc.iso8601)
     channel_videos = Fetch.channel_videos(url, 150, channelid) 
-    expect(channel_videos.values.flatten.count).to be(150)
+    expect(channel_videos.values.flatten.compact.count).to be(150)
   end
 end
