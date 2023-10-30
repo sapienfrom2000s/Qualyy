53d30effea0aab141ff51d2babbb498a0cbc160e
brickwall <queenisuseless@gmail.com>
2023-10-30 13:37:33 +0530

    Remove monkey patch
    - add a seperate class to handle addon functionalities of string

--- config/initializers/string_utils.rb
+++ config/initializers/string_utils.rb
@@ -1,15 +1,22 @@
-class String
+class StringUtils
+
+  attr_accessor :string
+
+  def initialize(string)
+    @string = string.to_str
+  end
+
   def keywords_present?(keywords)
     keywords.all? do |keyword|
       keyword = keyword.downcase
-      !(self.downcase.scan(/#{keyword}/).empty?)
+      !(string.downcase.scan(/#{keyword}/).empty?)
     end
   end
 
   def keywords_absent?(keywords)
     keywords.all? do |keyword|
       keyword = keyword.downcase
-      self.downcase.scan(/#{keyword}/).empty?
+      string.downcase.scan(/#{keyword}/).empty?
     end
   end
 end
--- spec/models/string_utils_spec.rb
+++ spec/models/string_utils_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe 'String' do
+RSpec.describe 'StringUtils' do
   describe "#keywords_present?" do
     it "is positive that keywords are present" do
       keywords = 'apple;sky'.split(';')
-      string = 'An apple in the sky'
+      string = StringUtils.new 'An apple in the sky'
       expect(string.keywords_present?(keywords)).to be true
     end
 
     it "is positive that keywords are not present" do
       keywords = 'apple;sky'.split(';')
-      string = 'An Apple in the Cloud'
+      string = StringUtils.new 'An Apple in the Cloud'
       expect(string.keywords_present?(keywords)).to be false
     end
 
     it "is positive that keywords are present" do
       keywords = 'official video;music'.split(';')
-      string = 'Official Video | Some Artist | Music Video'
+      string = StringUtils.new 'Official Video | Some Artist | Music Video'
       expect(string.keywords_present?(keywords)).to be true
     end
   end
@@ -24,19 +24,19 @@ RSpec.describe 'String' do
   describe "#keywords_absent?" do
     it "is positive that keywords are not absent" do
       keywords = 'apple;sky'.split(';')
-      string = 'An apple in the sky'
+      string = StringUtils.new 'An apple in the sky'
       expect(string.keywords_absent?(keywords)).to be false
     end
 
     it "is positive that keywords are not absent" do
       keywords = 'apple;sky'.split(';')
-      string = 'An Apple in the Cloud'
+      string = StringUtils.new 'An Apple in the Cloud'
       expect(string.keywords_absent?(keywords)).to be false
     end
 
     it "is positive that keywords are absent" do
       keywords = 'official video;music'.split(';')
-      string = 'Movie video | Some Actor | official'
+      string = StringUtils.new 'Movie video | Some Actor | official'
       expect(string.keywords_absent?(keywords)).to be true
     end
   end
