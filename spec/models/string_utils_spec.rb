fc812072e78ba22d38ee98d44ad40ec69d01c84e
brickwall <queenisuseless@gmail.com>
2023-10-30 15:52:34 +0530

    Run linter on unit tests

--- spec/models/string_utils_spec.rb
+++ spec/models/string_utils_spec.rb
@@ -1,40 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'StringUtils' do
-  describe "#keywords_present?" do
-    it "is positive that keywords are present" do
+  describe '#keywords_present?' do
+    it 'is positive that keywords are present' do
       keywords = 'apple;sky'.split(';')
       string = StringUtils.new 'An apple in the sky'
       expect(string.keywords_present?(keywords)).to be true
     end
 
-    it "is positive that keywords are not present" do
+    it 'is positive that keywords are not present' do
       keywords = 'apple;sky'.split(';')
       string = StringUtils.new 'An Apple in the Cloud'
       expect(string.keywords_present?(keywords)).to be false
     end
 
-    it "is positive that keywords are present" do
+    it 'is positive that keywords are present' do
       keywords = 'official video;music'.split(';')
       string = StringUtils.new 'Official Video | Some Artist | Music Video'
       expect(string.keywords_present?(keywords)).to be true
     end
   end
 
-  describe "#keywords_absent?" do
-    it "is positive that keywords are not absent" do
+  describe '#keywords_absent?' do
+    it 'is positive that keywords are not absent' do
       keywords = 'apple;sky'.split(';')
       string = StringUtils.new 'An apple in the sky'
       expect(string.keywords_absent?(keywords)).to be false
     end
 
-    it "is positive that keywords are not absent" do
+    it 'is positive that keywords are not absent' do
       keywords = 'apple;sky'.split(';')
       string = StringUtils.new 'An Apple in the Cloud'
       expect(string.keywords_absent?(keywords)).to be false
     end
 
-    it "is positive that keywords are absent" do
+    it 'is positive that keywords are absent' do
       keywords = 'official video;music'.split(';')
       string = StringUtils.new 'Movie video | Some Actor | official'
       expect(string.keywords_absent?(keywords)).to be true
--- spec/models/youtube_spec.rb
+++ spec/models/youtube_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe Youtube, type: :model do
     it 'fetches n number of videos from a channel' do
       # using sony music channel id
       channel_id = 'UC56gTxNs4f9xZ7Pa2i5xNzg'
-      videos = Youtube::Channel.videos(150, key: Rails.application.credentials.youtube_api_key, channelId: channel_id, publishedAfter: Date.new(2004,3,3).to_time.utc.iso8601)
+      videos = Youtube::Channel.videos(150, key: Rails.application.credentials.youtube_api_key, channelId: channel_id,
+                                            publishedAfter: Date.new(2004, 3, 3).to_time.utc.iso8601)
       expect(videos.values.flatten.compact.count).to be(150)
     end
   end
@@ -25,4 +26,3 @@ RSpec.describe Youtube, type: :model do
     end
   end
 end
-
