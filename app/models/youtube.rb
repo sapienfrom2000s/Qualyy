a191882089fe56f4f650f1e7bef552201d78a42f
brickwall <sk292207@gmail.com>
2024-01-11 12:56:00 +0530

    Use module functions instead of class methods

--- app/models/youtube.rb
+++ app/models/youtube.rb
@@ -1,9 +1,9 @@
 require 'faraday'
 require 'youtube_helper'
 
-class Youtube
-  class Channel
-    def self.videos(videos, **args)
+module Youtube
+  module Channel
+    def videos(videos, **args)
       url = url(args)
       video_ids = []
       response_object = fetch_data(url)
@@ -14,11 +14,11 @@ class Youtube
       { args[:channelId] => video_ids.flatten }
     end
 
-    def self.url(args, params = { maxResults: 50, type: :video, part: :snippet }.merge(args))
+    def url(args, params = { maxResults: 50, type: :video, part: :snippet }.merge(args))
       "https://www.googleapis.com/youtube/v3/search?#{params.map { |key, value| [key, value].join('=') }.join('&')}"
     end
 
-    def self.paginated_results(args)
+    def paginated_results(args)
       url = add_next_token_to_url(args[:next_page_token], args[:url])
       video_ids = []
       args[:pages].times do
@@ -29,33 +29,37 @@ class Youtube
       video_ids
     end
 
-    def self.add_next_token_to_url(token, url)
+    def add_next_token_to_url(token, url)
       url + "&pageToken=#{token}"
     end
 
-    def self.fetch_data(url)
+    def fetch_data(url)
       response = Faraday.get(url)
       JSON.parse(response.body)
     end
+
+    module_function :url, :fetch_data, :paginated_results, :add_next_token_to_url, :videos
   end
 
-  class Video
-    def self.metadata(video_id, api_key)
+  module Video
+    def metadata(video_id, api_key)
       data = fetch_data('https://www.googleapis.com/youtube/v3/videos' ,{:part => [:snippet, :contentDetails, :statistics], id: video_id, key: api_key })['items'][0]
       
       data['snippet'].merge data['contentDetails'], data['statistics']
     end
 
-    def self.dislikes(video_id)
+    def dislikes(video_id)
       fetch_data('https://returnyoutubedislikeapi.com/votes', {videoId: video_id})['dislikes']
     end
 
-    def self.fetch_data(url, params)
+    def fetch_data(url, params)
       connection = Faraday.new(
   url: ,request: { params_encoder: Faraday::FlatParamsEncoder },
   params: , headers: {'Content-Type' => 'application/json'}
 ) { |builder| builder.response :json }
       connection.get.body
     end
+
+    module_function :metadata, :dislikes, :fetch_data
   end
 end
