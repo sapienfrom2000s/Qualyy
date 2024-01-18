fa1242be78868d4f3cb2dea86eb44dffc076ec49
brickwall <sk292207@gmail.com>
2024-01-19 01:15:12 +0530

    Integrate hotwire, stimulus, modal
    - reintegrates hotwire
    - bit of stimulus(just a bit)
    - uses modal for channel entry
    - clean up
    - passes all the test

--- app/javascript/controllers/channel_form_controller.js
+++ app/javascript/controllers/channel_form_controller.js
@@ -5,14 +5,8 @@ export default class extends Controller {
     console.log('Hello World!')
   }
 
-  open(event){
-    event.preventDefault()
-    document.getElementsByClassName('modal')[0].classList.add('is-active')
-  }
-
   close(event){
     console.log('close triggered')
-    event.preventDefault()
     document.getElementsByClassName('modal')[0].classList.remove('is-active')
   }
 }
--- spec/system/channels_spec.rb
+++ spec/system/channels_spec.rb
@@ -15,7 +15,7 @@ feature 'user', type: :feature, js: true do
   it 'able to see fields' do
     sign_in user1
 
-    visit category_channels_path(category1)
+    visit category_path(category1)
 
     click_link 'Add Channel'
 
@@ -24,34 +24,34 @@ feature 'user', type: :feature, js: true do
       .and have_field('Maximum Time(in s)').and have_field('No of videos')
   end
 
-  it 'adds channel details' do
+  it 'adds channel details', js: true do
     sign_in user1
 
-    visit category_channels_path(category1)
+    visit category_path(category1)
 
     click_link 'Add Channel'
 
     fill_in 'Channel ID', with: 'abracadabra'
     fill_in 'Keywords', with: 'keyword1;keyword2'
     fill_in 'Non Keywords', with: 'nonkeyword1;nonkeyword2'
-    fill_in 'Published Before', with: '02122022'
-    fill_in 'Published After', with: '02112022'
+    fill_in 'Published Before', with: Date.new(2014, 01, 15)
     fill_in 'Minimum Time(in s)', with: 100
     fill_in 'Maximum Time(in s)', with: 1000
     select '50', from: 'No of videos'
     click_button 'Add'
 
+    page.current_window.resize_to(1600, 900)
+
     expect(page).to have_content('abracadabra').and have_content('keyword1;keyword2')
-      .and have_content('nonkeyword1;nonkeyword2').and have_content('12 Feb 2022')
-      .and have_content('11 Feb 2022').and have_content('00:01:40')
-      .and have_content('00:16:40').and have_content('50')
+      .and have_content('nonkeyword1;nonkeyword2').and have_content('15 Jan 2014')
+      .and have_content('00:01:40').and have_content('00:16:40').and have_content('50')
   end
 
   it 'deletes channel' do
     channel = create(:channel, user: user1, category: category1)
     sign_in user1
 
-    visit category_channels_path(category1)
+    visit category_path(category1)
 
     expect(page).to have_content(channel.identifier)
     page.current_window.resize_to(1600, 900)
@@ -65,31 +65,22 @@ feature 'user', type: :feature, js: true do
     channel = create(:channel, user: user1, category: category1)
     sign_in user1
 
-    visit category_channels_path(category1)
+    visit category_path(category1)
     page.current_window.resize_to(1600, 900)
-    click_link 'Edit'
 
-    expect(page).to have_field('Channel ID',
-                               with: channel.identifier).and have_field('Keywords', with: channel.keywords)
-      .and have_field('Non Keywords', with: channel.non_keywords).and have_field('Published Before', with: '2022-12-02')
-      .and have_field('Minimum Time(in s)',
-                      with: channel.minimum_duration).and have_field('Maximum Time(in s)',
-                                                                     with: channel.maximum_duration)
-      .and have_field('No of videos', with: channel.no_of_videos)
+    click_link "edit_channel_#{channel.id}"
 
     fill_in 'Channel ID', with: 'abracadabra'
     fill_in 'Keywords', with: 'keyword1;keyword2'
     fill_in 'Non Keywords', with: 'nonkeyword1;nonkeyword2'
-    fill_in 'Published Before', with: '02122022'
-    fill_in 'Published After', with: '02112022'
+    fill_in 'Published Before', with: Date.new(2014, 1, 14)
     fill_in 'Minimum Time(in s)', with: 100
     fill_in 'Maximum Time(in s)', with: 1000
     select '50', from: 'No of videos'
     click_button 'Update'
     
     expect(page).to have_content('abracadabra').and have_content('keyword1;keyword2')
-      .and have_content('nonkeyword1;nonkeyword2').and have_content('12 Feb 2022')
-      .and have_content('11 Feb 2022').and have_content('00:01:40')
+      .and have_content('nonkeyword1;nonkeyword2').and have_content('14 Jan 2014')
       .and have_content('00:16:40').and have_content('50')
   end
 end
