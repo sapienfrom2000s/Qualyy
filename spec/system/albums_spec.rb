8f7a0d86f13629b727a3515b29239897fbd5734a
brickwall <sk292207@gmail.com>
2024-01-26 17:03:29 +0530

    fix album specs
    - also makes changes to the tests to make it more robust

--- spec/system/albums_spec.rb
+++ spec/system/albums_spec.rb
@@ -2,25 +2,22 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Album', type: :feature do
+RSpec.describe 'Albums', type: :feature do
   include Devise::Test::IntegrationHelpers
 
-  let(:user1) { create(:user) }
-  let(:album1) { create(:album, user: user1) }
-  let(:album2) { create(:album, user: user1) }
-  let(:channel1) { create(:channel, album: album1, user: user1) }
-  let(:channel2) { create(:channel, album: album2, user: user1) }
+  let(:user) { create(:user) }
+  let(:album1) { create(:album, user:) }
+  let(:album2) { create(:album, user:) }
 
   before do
-    # bad code, defied the purpose of lazy loading
-    channel1 # touch
-    channel2 # touch
-    sign_in user1
+    create(:channel, album: album1)
+    create(:channel, album: album2)
+    sign_in user
     visit albums_path
   end
 
   it 'is/are listed to the current user' do
-    expect(page).to have_content('Music_1').and have_content('Music_2')
+    expect(page).to have_content(album1.name).and have_content(album2.name)
   end
 
   it 'can be added by the current user' do
@@ -34,7 +31,7 @@ RSpec.describe 'Album', type: :feature do
   it 'can be deleted by the current user' do
     click_on "delete_album_#{album1.id}"
 
-    expect(page).to have_no_content('Music_1')
+    expect(page).to have_no_content(album1.name)
   end
 
   it 'can be edited by the current user' do
